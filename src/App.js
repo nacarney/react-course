1d875c57be7333c0d6b77d3705d3c465e8a265be
nacarney <nacarney@tcd.ie>
2022-01-21 19:24:54 +0000

    Added routing components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+// importing Routes as Switch component is outdated, as per YT comment under video
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  // domain eg is localhost:3000 or my-page.com
+  // path eg is localhost:3000/products or my-page.com/products
+  // to use nested components (every change of page just adds a new component), format is: 
+  // <Route path = '/'> <AllMeetupsPage /> <Route>
+  // <Route path = '/favorites'> <FavoritesPage /> <Route>
+
+  return (
+    <div>
+       <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
-
