44a510f482efd07c07840b082da2b61137622da2
nacarney <nacarney@tcd.ie>
2022-01-21 20:32:52 +0000

    Using a list component to output data

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,15 +1,21 @@
-function MeetupItem(props){
-    return <li>
-        <div>
-            <img src = {props.image} alt = {props.title} />
-        </div>
-        <div>
-            <h3>{props.title}</h3>
-            <address>{props.address}</address>
-            <p>{props.description}</p>
-        </div>
-        <div>
-            <button>To Favorites</button>
-        </div>
+import classes from "./MeetupItem.module.css";
+
+function MeetupItem(props) {
+  return (
+    <li className={classes.item}>
+      <div className={classes.image}>
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className={classes.content}>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+      </div>
+      <div className={classes.actions}>
+        <button>To Favorites</button>
+      </div>
     </li>
+  );
 }
+
+export default MeetupItem;
