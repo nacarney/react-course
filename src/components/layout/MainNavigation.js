9ef84f1f844016985b2cfb12dc6a49d147a82f95
nacarney <nacarney@tcd.ie>
2022-01-21 19:36:37 +0000

    Link Component

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -6,7 +6,13 @@ return <header>
     <nav>
         <ul>
             <li>
-                <Link>Some Link</Link>
+                <Link to = '/'>All Meetups</Link>
+            </li>
+            <li>
+                <Link to = '/new-meetup'>New Meetup</Link>
+            </li>
+            <li>
+                <Link to = '/favorites'>Favorites</Link>
             </li>
         </ul>
     </nav>
