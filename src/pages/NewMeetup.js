d4613ae2ccb320f46fc7cc03cb0f2bafeb4395f7
nacarney <nacarney@tcd.ie>
2022-01-22 17:51:00 +0000

    navigating pages upon completed actions

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,16 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+  const navigate = useNavigate();
+
   function addMeetupHandler(meetupData) {
-    fetch('https://react-getting-started-7d347-default-rtdb.firebaseio.com/meetups.json',
-    {
-      method: 'POST',
-      body: JSON.stringify(meetupData),
-      headers: {
-        'Content-Type': 'application/json' 
+    fetch(
+      "https://react-getting-started-7d347-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-    }
-    );
+    ).then(() => {
+      navigate("/", { replace: true });
+    });
   }
   return (
     <section>
