eb79626274e8e3155c2ea34035e9bb1aa1de88fc
nacarney <nacarney@tcd.ie>
2022-01-31 13:33:24 +0000

    set up favorites functionality using context

---
+++ src/store/favorites-context.js
@@ -0,0 +1,46 @@
+import { createContext, useState } from "react";
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+});
+
+function FavoritesContextProvider(props) {
+
+    const [userFavorites, setUserFavorites] = useState([]);
+
+    function addFavoriteHandler(favoriteMeetup){
+        // React automatically receives the previous state snapshot when calling the state updating function, 
+        // in this case prevUserFavorites and setUserFavorites 
+        setUserFavorites((prevUserFavorites) => {
+            // prevUserFavorites = old set of favorites (previous state snapshot), 
+            // favoriteMeetup = new meetup that has been passed as a parameter to addFavouriteHandler()
+            return prevUserFavorites.concat(favoriteMeetup);
+        });
+    }
+
+    // meetupId identifies the meetup to be removed
+    function removeFavoriteHandler(meetupId){
+        setUserFavorites(prevUserFavorites => {
+            // .filter() returns a new array which has a value taken out of an old array, it takes a function as an argument which 
+            // executes for each item in that array and we get that item (meetup) as a parameter, return true if we want to keep that item
+            // and false if we want to get rid of it
+            // if meetup.id is equal to meetupId, then the function returns false and the item is removed
+            return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
+        });
+    }
+
+    function itemIsFavoriteHandler(meetupId){
+        return userFavorites.some(meetup => meetup.id == meetupId );
+    }
+
+    const context = {
+        favorites: userFavorites,
+        totalFavorites = userFavorites.length
+    };
+
+
+  return (
+    <FavoritesContext.Provider>{props.children}</FavoritesContext.Provider>
+  );
+}
